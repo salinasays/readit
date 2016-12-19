2830c4b27f352c6716b52ef2274ef28a614c288d
Ruben Gil <gil.r419@gmail.com>
2016-12-19 16:25:37 -0500

    added comment routes

--- routes/api.js
+++ routes/api.js
@@ -24,6 +24,37 @@ const deletePost = (req, res) => {
   Post.destroy({where: {id: req.params.id}})
 }
 
+const getComments = (req, res) => {
+  Post.findById(req.params.id)
+   .then((post) => {
+     return post.getComments()
+   })
+   .then((comments) => {
+     res.send(comments)
+   })
+}
+
+const createComment = (req, res) => {
+  //format of post should be {comment}
+
+  let newComment = {
+    comment: req.body.comment,
+    PostId: req.params.id
+  }
+
+  Comment.create(newComment)
+    .then((comment) => {
+      Post.findById(req.params.id)
+            .then((post) => {
+              post.addComments([comment]);
+              res.send(comment);
+            })
+    })
+}
+
+const deleteComment = (req, res) => {
+  Comment.destroy({where: {id: req.params.id}});
+}
 
 router.route('/posts')
   .get(getPosts)
@@ -31,5 +62,12 @@ router.route('/posts')
 
 router.route('/posts/:id')
   .delete(deletePost)
+
+router.route('/posts/:id/comments')
+  .get(getComments)
+  .post(createComment)
+
+router.route('/comments/:id')
+  .delete(deleteComment)
 
 module.exports = router
